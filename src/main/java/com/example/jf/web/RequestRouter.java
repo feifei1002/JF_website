6e731a5df2de55228156a8c9c67d1938f4098150
feifei1002 <liufeipek@gmail.com>
2023-07-05 15:57:54 +0700

    added vietnamese sites

--- src/main/java/com/example/jf/web/RequestRouter.java
+++ src/main/java/com/example/jf/web/RequestRouter.java
@@ -8,10 +8,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class RequestRouter implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/index").setViewName("index");
+        registry.addViewController("/").setViewName("index-en");
+        registry.addViewController("/home-en").setViewName("index-en");
         registry.addViewController("/about-en").setViewName("about-en");
         registry.addViewController("/contact-en").setViewName("contact-en");
         registry.addViewController("/products-en").setViewName("products-en");
-        registry.addViewController("/fragments/fragment").setViewName("fragment");
+        registry.addViewController("/fragments/fragment-en").setViewName("fragment-en");
+        registry.addViewController("/home-vn").setViewName("index-vn");
+        registry.addViewController("/about-vn").setViewName("about-vn");
+        registry.addViewController("/contact-vn").setViewName("contact-vn");
+        registry.addViewController("/products-vn").setViewName("products-vn");
+        registry.addViewController("/fragments/fragment-vn").setViewName("fragment-vn");
     }
 }
